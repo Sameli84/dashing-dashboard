2233a529d1544bdc92ab2452e07b163c1803e0cb
Sami Saastamoinen <sami.saastamoinen@tuni.fi>
2023-03-16 11:23:45 +0200

    fix: add itemContainer view

--- components/screens/widgets/ScreenStepLogger.js
+++ components/screens/widgets/ScreenStepLogger.js
@@ -16,81 +16,83 @@ const ScreenStepLogger = ({ navigation }) => {
 					<IconButton icon={require(settings)} size={60} onPress={() => navigation.navigate('Settings')} />
 				</View>
 			</View>
-			<View style={styles.itemView}>
-				<Text style={styles.timeText} variant='headlineMedium'>
-					Today
-				</Text>
-				<Text style={styles.stepText} variant='headlineMedium'>
-					1000 steps
-				</Text>
-				<View style={styles.imageView}>
-					<Image style={styles.image} source={require(smiley)} />
+			<View style={styles.itemContainer}>
+				<View style={styles.itemView}>
+					<Text style={styles.timeText} variant='headlineMedium'>
+						Today
+					</Text>
+					<Text style={styles.stepText} variant='headlineMedium'>
+						1000 steps
+					</Text>
+					<View style={styles.imageView}>
+						<Image style={styles.image} source={require(smiley)} />
+					</View>
 				</View>
-			</View>
-			<View style={styles.itemView}>
-				<Text style={styles.timeText} variant='headlineMedium'>
-					Yesterday
-				</Text>
-				<Text style={styles.stepText} variant='headlineMedium'>
-					3500 steps
-				</Text>
-				<View style={styles.imageView}>
-					<Image style={styles.image} source={require(smiley)} />
+				<View style={styles.itemView}>
+					<Text style={styles.timeText} variant='headlineMedium'>
+						Yesterday
+					</Text>
+					<Text style={styles.stepText} variant='headlineMedium'>
+						3500 steps
+					</Text>
+					<View style={styles.imageView}>
+						<Image style={styles.image} source={require(smiley)} />
+					</View>
 				</View>
-			</View>
-			<View style={styles.itemView}>
-				<Text style={styles.timeText} variant='headlineMedium'>
-					This week
-				</Text>
-				<Text style={styles.stepText} variant='headlineMedium'>
-					20000 steps
-				</Text>
-				<View style={styles.imageView}>
-					<Image style={styles.image} source={require(smiley)} />
+				<View style={styles.itemView}>
+					<Text style={styles.timeText} variant='headlineMedium'>
+						This week
+					</Text>
+					<Text style={styles.stepText} variant='headlineMedium'>
+						20000 steps
+					</Text>
+					<View style={styles.imageView}>
+						<Image style={styles.image} source={require(smiley)} />
+					</View>
 				</View>
-			</View>
-			<View style={styles.itemView}>
-				<Text style={styles.timeText} variant='headlineMedium'>
-					Last week
-				</Text>
-				<Text style={styles.stepText} variant='headlineMedium'>
-					18000 steps
-				</Text>
-				<View style={styles.imageView}>
-					<Image style={styles.image} source={require(smiley)} />
+				<View style={styles.itemView}>
+					<Text style={styles.timeText} variant='headlineMedium'>
+						Last week
+					</Text>
+					<Text style={styles.stepText} variant='headlineMedium'>
+						18000 steps
+					</Text>
+					<View style={styles.imageView}>
+						<Image style={styles.image} source={require(smiley)} />
+					</View>
 				</View>
-			</View>
-			<View style={styles.itemView}>
-				<Text style={styles.timeText} variant='headlineMedium'>
-					This month
-				</Text>
-				<Text style={styles.stepText} variant='headlineMedium'>
-					80000 steps
-				</Text>
-				<View style={styles.imageView}>
-					<Image style={styles.image} source={require(smiley)} />
+				<View style={styles.itemView}>
+					<Text style={styles.timeText} variant='headlineMedium'>
+						This month
+					</Text>
+					<Text style={styles.stepText} variant='headlineMedium'>
+						80000 steps
+					</Text>
+					<View style={styles.imageView}>
+						<Image style={styles.image} source={require(smiley)} />
+					</View>
 				</View>
-			</View>
-			<View style={styles.itemView}>
-				<Text style={styles.timeText} variant='headlineMedium'>
-					This year
-				</Text>
-				<Text style={styles.stepText} variant='headlineMedium'>
-					900k steps
-				</Text>
-				<View style={styles.imageView}>
-					<Image style={styles.image} source={require(smiley)} />
+				<View style={styles.itemView}>
+					<Text style={styles.timeText} variant='headlineMedium'>
+						This year
+					</Text>
+					<Text style={styles.stepText} variant='headlineMedium'>
+						900k steps
+					</Text>
+					<View style={styles.imageView}>
+						<Image style={styles.image} source={require(smiley)} />
+					</View>
 				</View>
-			</View>
-			<View style={styles.itemView}>
-				<Text style={styles.timeText} variant='headlineMedium'>
-					Last year
-				</Text>
-				<Text style={styles.stepText} variant='headlineMedium'>
-					890k steps
-				</Text>
-				<View style={styles.imageView}>
-					<Image style={styles.image} source={require(smiley)} />
+				<View style={styles.itemView}>
+					<Text style={styles.timeText} variant='headlineMedium'>
+						Last year
+					</Text>
+					<Text style={styles.stepText} variant='headlineMedium'>
+						890k steps
+					</Text>
+					<View style={styles.imageView}>
+						<Image style={styles.image} source={require(smiley)} />
+					</View>
 				</View>
 			</View>
 		</View>
@@ -115,6 +117,9 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		overflow: 'hidden',
 		padding: 2,
+	},
+	itemContainer: {
+		flex: 8,
 	},
 	itemView: {
 		flex: 1,
