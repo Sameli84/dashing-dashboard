cb4c9cb301b53161fcf7e973ebae9cf2631ae8f5
Sami Saastamoinen <sami.saastamoinen@tuni.fi>
2023-03-16 10:19:41 +0200

    feat: add step logger screen base

--- App.js
+++ App.js
@@ -4,21 +4,23 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ScreenDashboard from './components/screens/ScreenDashboard';
 import { Provider as PaperProvider } from 'react-native-paper';
 import ScreenSettings from './components/screens/ScreenSettings';
+import ScreenStepLogger from './components/screens/widgets/ScreenStepLogger';
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
-  return (
-    // setup react-native-paper and react-navigator context
-    <PaperProvider>
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name='Dashboard' component={ScreenDashboard} />
-          <Tab.Screen name='Settings' component={ScreenSettings} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </PaperProvider>
-  );
+	return (
+		// setup react-native-paper and react-navigator context
+		<PaperProvider>
+			<NavigationContainer>
+				<Tab.Navigator>
+					<Tab.Screen name='Dashboard' component={ScreenDashboard} />
+					<Tab.Screen name='StepLogger' component={ScreenStepLogger} />
+					<Tab.Screen name='Settings' component={ScreenSettings} />
+				</Tab.Navigator>
+			</NavigationContainer>
+		</PaperProvider>
+	);
 };
 
 export default App;
---
+++ components/screens/widgets/ScreenStepLogger.js
@@ -0,0 +1,147 @@
+import { React } from 'react';
+import { View, StyleSheet, Image } from 'react-native';
+import { IconButton, Card, Text } from 'react-native-paper';
+
+const ScreenStepLogger = ({ navigation }) => {
+	const smiley = '../../../assets/smiley.png';
+	const settings = '../../../assets/settings.png';
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.titleView}>
+				<Text style={styles.titleText} variant='headlineLarge'>
+					Step Logger
+				</Text>
+				<View style={styles.imageView}>
+					<IconButton icon={require(settings)} size={60} onPress={() => navigation.navigate('Settings')} />
+				</View>
+			</View>
+			<View style={styles.itemView}>
+				<Text style={styles.timeText} variant='headlineMedium'>
+					Today
+				</Text>
+				<Text style={styles.stepText} variant='headlineMedium'>
+					1000 steps
+				</Text>
+				<View style={styles.imageView}>
+					<Image style={styles.image} source={require(smiley)} />
+				</View>
+			</View>
+			<View style={styles.itemView}>
+				<Text style={styles.timeText} variant='headlineMedium'>
+					Yesterday
+				</Text>
+				<Text style={styles.stepText} variant='headlineMedium'>
+					3500 steps
+				</Text>
+				<View style={styles.imageView}>
+					<Image style={styles.image} source={require(smiley)} />
+				</View>
+			</View>
+			<View style={styles.itemView}>
+				<Text style={styles.timeText} variant='headlineMedium'>
+					This week
+				</Text>
+				<Text style={styles.stepText} variant='headlineMedium'>
+					20000 steps
+				</Text>
+				<View style={styles.imageView}>
+					<Image style={styles.image} source={require(smiley)} />
+				</View>
+			</View>
+			<View style={styles.itemView}>
+				<Text style={styles.timeText} variant='headlineMedium'>
+					Last week
+				</Text>
+				<Text style={styles.stepText} variant='headlineMedium'>
+					18000 steps
+				</Text>
+				<View style={styles.imageView}>
+					<Image style={styles.image} source={require(smiley)} />
+				</View>
+			</View>
+			<View style={styles.itemView}>
+				<Text style={styles.timeText} variant='headlineMedium'>
+					This month
+				</Text>
+				<Text style={styles.stepText} variant='headlineMedium'>
+					80000 steps
+				</Text>
+				<View style={styles.imageView}>
+					<Image style={styles.image} source={require(smiley)} />
+				</View>
+			</View>
+			<View style={styles.itemView}>
+				<Text style={styles.timeText} variant='headlineMedium'>
+					This year
+				</Text>
+				<Text style={styles.stepText} variant='headlineMedium'>
+					900k steps
+				</Text>
+				<View style={styles.imageView}>
+					<Image style={styles.image} source={require(smiley)} />
+				</View>
+			</View>
+			<View style={styles.itemView}>
+				<Text style={styles.timeText} variant='headlineMedium'>
+					Last year
+				</Text>
+				<Text style={styles.stepText} variant='headlineMedium'>
+					890k steps
+				</Text>
+				<View style={styles.imageView}>
+					<Image style={styles.image} source={require(smiley)} />
+				</View>
+			</View>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexDirection: 'column',
+		backgroundColor: '#def9f4',
+	},
+	image: {
+		width: '100%',
+		height: '100%',
+		resizeMode: 'contain',
+		borderRadius: 15,
+		backgroundColor: '#def9f4',
+	},
+	imageView: {
+		flex: 2,
+		justifyContent: 'center',
+		overflow: 'hidden',
+		padding: 2,
+	},
+	itemView: {
+		flex: 1,
+		flexDirection: 'row',
+		backgroundColor: '#85d3c5',
+		margin: 5,
+		borderRadius: 10,
+	},
+	stepText: {
+		flex: 3,
+		textAlignVertical: 'center',
+	},
+	timeText: {
+		flex: 5,
+		textAlignVertical: 'center',
+		padding: 5,
+	},
+	titleView: {
+		flex: 1,
+		flexDirection: 'row',
+		margin: 5,
+	},
+	titleText: {
+		flex: 8,
+		textAlignVertical: 'center',
+		paddingLeft: 7,
+	},
+});
+
+export default ScreenStepLogger;
