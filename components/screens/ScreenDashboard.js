d502709e8260d2f85e9b293e01cff8821a8d58eb
marttak <martta.korpi@tuni.fi>
2023-03-28 21:10:49 +0300

    fix: removed vscode merge editor mess from ScreenDashboard.js

--- components/screens/ScreenDashboard.js
+++ components/screens/ScreenDashboard.js
@@ -1,41 +1,12 @@
-<<<<<<< 0e1c07209fd4555361c9e6fecfe6bff1c627d886
-<<<<<<< 98d7702eb4597b6d7bee25905eb1dae3f9e52da3
 import { React, useState } from 'react';
-=======
-import { useEffect } from 'react';
->>>>>>> chore: added get, add and delete functionalities for Todos and Feelings
 import { View } from 'react-native';
 import { Text } from 'react-native-paper';
 import { constants } from '../../styles/style'; // feel free to add styles
 import MoodWidgetPart from '../MoodLogger/MoodWidgetPart';
-import * as backend from '../backend/backend';
-=======
-import { View } from 'react-native';
-import { Text } from 'react-native-paper';
-import { constants } from '../../styles/style'; // feel free to add styles
->>>>>>> chore: add edit functionality for Feelings and Todos
 
 // screens get 'navigation' as prop from context
 const ScreenDashboard = ({ navigation, route }) => {
   const userName = 'Test';
-<<<<<<< 0e1c07209fd4555361c9e6fecfe6bff1c627d886
-<<<<<<< 98d7702eb4597b6d7bee25905eb1dae3f9e52da3
-<<<<<<< 614261bb4f6408537818a4698ae44c295e9048cc
-=======
-  backend.getTodos(); // testing backend layer here
->>>>>>> fix: firestore backend layer works
-=======
-  useEffect(() => {
-    const getData = async () => {
-      const feelings = await backend.getFeelingsByDate(new Date('2023-03-27'));
-      console.log(feelings[2]);
-      await backend.deleteFeeling(new Date('2023-03-27'), feelings[1]);
-    };
-    getData();
-  }, []);
->>>>>>> chore: added get, add and delete functionalities for Todos and Feelings
-=======
->>>>>>> chore: add edit functionality for Feelings and Todos
   return (
     <View style={{ flex: 1, flexDirection: 'column', backgroundColor: '#def9f4' }}>
       <Text style={{ flex: 1, textAlignVertical: 'center', paddingLeft: 7 }} variant='headlineLarge'>
