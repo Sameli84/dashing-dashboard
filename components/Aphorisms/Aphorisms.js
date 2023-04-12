d0f5f333b7df5fa38babb4c2512618f60c2cbb19
Sampsa Jarvinen <Sampsa.Jarvinen@tuni.fi>
2023-04-12 10:09:36 +0300

    feat: add Inspiratioid of the Day to Dashboard

---
+++ components/Aphorisms/Aphorisms.js
@@ -0,0 +1,50 @@
+import { React, useState, useEffect } from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+import * as Font from 'expo-font';
+import { constants } from '../../styles/style';
+
+const Aphorisms = () => {
+  useEffect(() => {
+    (async () =>
+      await Font.loadAsync({
+        MorrisRoman: require('../../assets/fonts/MorrisRoman-Black.ttf'),
+      }))();
+  }, []);
+
+  const [aphorism, setAphorism] = useState({ q: 'Inspiration incoming...', a: '' });
+
+  const options = {
+    method: 'GET',
+  };
+
+  useEffect(() => {
+    const asyncToday = async () => {
+      fetch('https://zenquotes.io/api/quotes/', options)
+        .then((response) => response.json())
+        .then((response) => {
+          setAphorism(response[Math.floor(Math.random() * 50)]);
+        })
+        .catch((err) => console.error(err));
+    };
+    asyncToday();
+  }, []);
+
+  return (
+    <View style={{ marginTop: constants.SPACER_2, flex: 3 }}>
+      <View style={{ flex: 1 }}>
+        <Text style={{ textAlignVertical: 'center', paddingLeft: 7 }} variant='headlineLarge'>
+          Inspiration of the Day
+        </Text>
+      </View>
+      <View style={{ flex: 2 }}>
+        <Text style={{ textAlignVertical: 'center', paddingLeft: 7, fontFamily: 'MorrisRoman', fontSize: 30}} >
+          {aphorism['q']}
+        </Text>
+        <Text style={{ textAlignVertical: 'center', paddingLeft: 7, fontFamily: 'MorrisRoman', fontSize: 24 }}>- {aphorism['a']}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Aphorisms;
--- components/screens/ScreenDashboard.js
+++ components/screens/ScreenDashboard.js
@@ -4,21 +4,13 @@ import { Text } from 'react-native-paper';
 import { constants } from '../../styles/style'; // feel free to add styles
 import MoodWidgetPart from '../MoodLogger/MoodWidgetPart';
 import TodoWidgetPart from '../TodoList/TodoWidgetPart';
+import Aphorisms from '../Aphorisms/Aphorisms';
 import { auth } from '../../config/firebase';
 import style from '../../styles/style';
 
 // screens get 'navigation' as prop from context
 const ScreenDashboard = ({ navigation, route }) => {
   const [userName, setUserName] = useState('No one');
-  const aphorisms = [
-    'Code as if someone else will have to maintain your work, because one day they will.',
-    "A web developer's code is only as good as their commitment to testing and debugging.",
-    'A successful website is built not just with code, but with a deep understanding of user experience.',
-    'The best web developers never stop learning, for the internet is always changing.',
-    'Code is like poetry, every line must be intentional and every character meaningful.',
-  ];
-
-  let aphorismOfTheDay = aphorisms[Math.floor(Math.random() * aphorisms.length)]
 
   // try to set the name after auth but doesn't work atm
   useEffect(() => {
@@ -36,10 +28,10 @@ const ScreenDashboard = ({ navigation, route }) => {
     <View style={style.containerLeftAligned}>
       <Text variant='headlineLarge'>Hi {userName}!</Text>
       <Text variant='titleLarge'>Widgets</Text>
-      <View style={{ flex: 3, marginTop: constants.SPACER_3, width: '100%' }}>
+      <View style={{ flex: 3, marginTop: constants.SPACER_2, width: '100%' }}>
         <MoodWidgetPart navigation={navigation} route={route}></MoodWidgetPart>
         <TodoWidgetPart navigation={navigation} route={route}></TodoWidgetPart>
-        <View style={{ flex: 3 }}><Text variant='bodyLarge'>{aphorismOfTheDay}</Text></View>
+        <Aphorisms></Aphorisms>
       </View>
     </View>
   );
